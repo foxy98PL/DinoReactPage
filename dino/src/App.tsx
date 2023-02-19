30b0794664a5c26005de6475183a75cfb0ce16eb
Piotrek <piotrflorek@op.pl>
2023-02-19 19:35:46 +0100

    added ranking, loader and changed table

--- dino/src/App.tsx
+++ dino/src/App.tsx
@@ -3,12 +3,9 @@ import DinoTable from "./components/DinoTable/DinoTable";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-  const dinoLeader = require("./assetsDino/dinoLeader.png");
-  const dinoTail = require("./assetsDino/dinoTail.png");
   return (
     <>
       <div className="dinoPage">
-        <div className="dinoPageTitle">Dino Leaderboard</div>
         <DinoTable />
       </div>
       <Footer />
--- dino/src/components/DinoTable/DinoTable.tsx
+++ dino/src/components/DinoTable/DinoTable.tsx
@@ -4,6 +4,8 @@ import "./DinoTable.scss";
 import { DinoTableModel } from "./model";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Rank from "../RankComponent/Rank";
+import Loader from "../Loader/Loader";
 
 const DinoTable = () => {
   const [data, setData] = useState<any>(null);
@@ -37,38 +39,49 @@ const DinoTable = () => {
   const dinoTail = require("../../assetsDino/dinoTail.png");
 
   return (
-    <div className="dinoTable_wrapper">
-      <div className="dinoFull">
-        <img className="dinoTail" alt="tail" src={dinoTail} />
-        <img className="dinoLeader" alt="character" src={dinoLeader} />
-      </div>
-      <div className="dinoTable">
-        <div className="row header">
-          <div className="cell">Wallet</div>
-          <div className="cell">Total ETH</div>
-          <div className="cell">Buy Count</div>
-          <div className="cell">RANK</div>
-        </div>
-        {dinoData.map((item: DinoTableModel, index: number) => {
-          return (
-            <div className="row">
-              <div
-                className="cell"
-                onClick={() => handleCopy(item.walletaddress)}
-              >
-                {item.walletaddress}
-              </div>
-              <div className="cell">{item.ethervalue}</div>
-              <div className="cell">{item.value}</div>
-              <div className="cell">{index + 1}</div>
+    <>
+      {isLoading && (
+        <div className="dinoTable_wrapper">
+          <div className="dinoFull">
+            <img className="dinoTail" alt="tail" src={dinoTail} />
+            <img className="dinoLeader" alt="character" src={dinoLeader} />
+          </div>
+          <div className="dinoTable">
+            <div className="row header" id="row_header">
+              <div className="cell">Wallet</div>
+              <div className="cell">Total ETH</div>
+              <div className="cell">Buy Count</div>
+              <div className="cell">RANK</div>
             </div>
-          );
-        })}
-      </div>
-      <ToastContainer
-        toastStyle={{ backgroundColor: "#38625a", color: "#fff" }}
-      />
-    </div>
+          </div>
+          <div className="dinoTable_2_wrapper">
+            <div className="dinoTable">
+              {dinoData.map((item: DinoTableModel, index: number) => {
+                return (
+                  <div className="row" key={index}>
+                    <div
+                      className="cell"
+                      onClick={() => handleCopy(item.walletaddress)}
+                    >
+                      {item.walletaddress}
+                    </div>
+                    <div className="cell">{item.ethervalue}</div>
+                    <div className="cell">{item.value}</div>
+                    <div className="cell">
+                      <Rank score={index + 1} />
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <ToastContainer
+            toastStyle={{ backgroundColor: "#38625a", color: "#fff" }}
+          />
+        </div>
+      )}
+      {!isLoading && <Loader />}
+    </>
   );
 };
 
