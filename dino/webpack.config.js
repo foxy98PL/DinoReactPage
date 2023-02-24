c935788e9e6c966db1fe1ff62d68a59cfdc4465b
Tomasz Cieśliński <tomasz.cieslinski@sap.com>
2023-02-25 00:14:15 +0100

    Add webpack

---
+++ dino/webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const InterpolateHtmlPlugin = require('interpolate-html-plugin')
+
+module.exports = {
+    mode: 'none',
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx')
+    },
+    target: 'web',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: [
+                      "@babel/preset-env",
+                      "@babel/preset-react",
+                      "@babel/preset-typescript",
+                    ],
+                  },
+                },
+            },{
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+            },{
+                test: /\.(jpe?g|png|gif|svg)$/i, 
+                loader: 'file-loader',
+                options: {
+                  name: '/public/icons/[name].[ext]'
+                }
+            },  {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+              },
+              { test: /\.txt$/, 
+                use: ['raw-loader'] 
+              },
+              { test: /\.html$/, 
+                use: ['html-loader'] 
+              },
+              {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+                exclude: /node_modules/,
+                use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
+              }
+            
+        ]
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: "[name].js",
+      sourceMapFilename: "[name].js.map"
+    },
+    devtool: "source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        }),
+        new InterpolateHtmlPlugin({
+            PUBLIC_URL: 'static' // can modify `static` to another name or get it from `process`
+        }),
+    ]
+    
+}
